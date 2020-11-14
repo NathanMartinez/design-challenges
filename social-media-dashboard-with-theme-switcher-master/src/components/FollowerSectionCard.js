8928037897c0e80ae2fc74ea857cb2c97467bb12
Nate Martinez <natemartinez@Nates-iMac.local>
2020-11-13 19:02:57 -0700

    Complteted the Follower Section Cards, I added all required props to the component so that it displays the proper icon, top border, and text

--- social-media-dashboard-with-theme-switcher-master/src/components/FollowerSectionCard.js
+++ social-media-dashboard-with-theme-switcher-master/src/components/FollowerSectionCard.js
@@ -1,24 +1,39 @@
 import React from "react";
 import "../styles/FollowerSectionCard.css";
 
-function FollowerSectionCard({ darkMode, setDarkMode }) {
+function FollowerSectionCard({
+  darkMode,
+  setDarkMode,
+  platform,
+  username,
+  followers,
+  updateDirection,
+  updateCount,
+}) {
   return (
     <div
       className={
-        darkMode ? "follower-section-card dark-mode" : "follower-section-card"
+        darkMode ? `follower-section-card dark-mode` : `follower-section-card`
       }
     >
+      <div
+        className={darkMode ? `heading ${platform}` : `heading ${platform}`}
+      ></div>
       <div className="icon-username-container">
-        <img src="./images/icon-facebook.svg" alt="social-icon"></img>
-        <p>NathanMartinez</p>
+        <img src={`./images/icon-${platform}.svg`} alt="social-icon"></img>
+        <p>{username}</p>
       </div>
       <div className="follower-count-container">
-        <h1>3000</h1>
-        <h4>followers</h4>
+        <h1>{followers}</h1>
+        <h4>{platform === "youtube" ? "subscribers" : "followers"}</h4>
       </div>
-      <div className="update-info">
-        <img src="./images/icon-up.svg" alt="up" />
-        <p>99 Today</p>
+      <div
+        className={
+          updateDirection === "up" ? "update-info up" : "update-info down"
+        }
+      >
+        <img src={`./images/icon-${updateDirection}.svg`} alt="up" />
+        <p>{updateCount} Today</p>
       </div>
     </div>
   );
--- social-media-dashboard-with-theme-switcher-master/src/components/FollowersSection.js
+++ social-media-dashboard-with-theme-switcher-master/src/components/FollowersSection.js
@@ -6,7 +6,42 @@ function FollowersSection({ darkMode, setDarkMode }) {
     <div
       className={darkMode ? "follwers-section dark-mode" : "followers-section"}
     >
-      <FollowerSectionCard darkMode={darkMode} setDarkMode={setDarkMode} />
+      <FollowerSectionCard
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}
+        platform="facebook"
+        username="Nate Martinez"
+        followers="1294"
+        updateDirection="up"
+        updateCount="1653"
+      />
+      <FollowerSectionCard
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}
+        platform="twitter"
+        username="@NateMtz"
+        followers="2394"
+        updateDirection="up"
+        updateCount="1243"
+      />
+      <FollowerSectionCard
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}
+        platform="instagram"
+        username="NateMtz"
+        followers="1294"
+        updateDirection="up"
+        updateCount="23"
+      />
+      <FollowerSectionCard
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}
+        platform="youtube"
+        username="Nate M."
+        followers="2894"
+        updateDirection="down"
+        updateCount="12"
+      />
     </div>
   );
 }
