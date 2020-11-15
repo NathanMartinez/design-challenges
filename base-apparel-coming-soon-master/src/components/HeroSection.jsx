e07f5bc65e536d81fdab477a42f58c6d647c6b13
Nate Martinez <natemartinez@Nates-iMac.local>
2020-11-14 18:22:29 -0700

    completed Desktop UI and made the site responsive

--- base-apparel-coming-soon-master/src/components/HeroSection.jsx
+++ base-apparel-coming-soon-master/src/components/HeroSection.jsx
@@ -3,7 +3,8 @@ import React from "react";
 function HeroSection(props) {
   return (
     <div className="Hero-section">
-      <img src="./images/hero-mobile.jpg" alt="hero-image" />
+      <img id="mobile" src="./images/hero-mobile.jpg" alt="hero-image" />
+      <img id="desktop" src="./images/hero-desktop.jpg" alt="hero-image" />
     </div>
   );
 }
